4efcd6f46502d792e86c63bc837cf9d545f5be50
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2019-02-12 22:28:14 -0500

    rewrote extension to cli docker and git ignore

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,20 +1,20 @@
-# services/users/manage.py
+# services/user/manage.py
 
 from flask.cli import FlaskGroup
 
 from project import app, db #new
 
+cli = FlaskGroup(app)
 
-cli = FlaskGroup(app) #created a new FlaskGroup instance to extend the normal CLI with command related to the Flask App
-
-#new
-cli.command('recreate_db')
+# new
+#This register a new command recreate_db, to the cli
+#so that we can run it from CLI, which we'll use shortlly to apply the model to the database
+@cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
