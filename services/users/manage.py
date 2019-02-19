013217b7e8d4a76bfe2f46683da24ce985cc5672
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2019-02-18 20:02:13 -0500

    part 1 completed

--- services/users/manage.py
+++ services/users/manage.py
@@ -28,6 +28,16 @@ def test():
         return 0
     return 1
 
+# seed comand to populate the database with some initial data:
+@cli.command('seed_db')
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='jeffrey', email='monsalve.jeffrey@gmail.com'))
+    db.session.add(User(username='michael', email='hermanmu@gmail.com'))
+    db.session.commit()
+
+
+
 
 if __name__ == '__main__':
     cli()
