ea6633450e3fbbd3112dff38c74da23f09fc174a
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2019-03-06 21:11:18 -0500

    React

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';    //  new
 import ReactDOM from 'react-dom';   // import ReactDom
 import axios from 'axios';    // new
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 
@@ -12,9 +13,36 @@ class App extends Component {
     super();
     // new
     this.state = {
-      users: []
+      users: [],
+      username: 'test', // new
+      email: 'test', // new
     };
+    this.addUser = this.addUser.bind(this); //new
+    this.handleChange = this.handleChange.bind(this);
   };
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+
+  addUser(event) {
+    event.preventDefault();
+    // new
+    const data ={
+      username: this.state.username,
+      email: this.state.email
+    };
+    //new
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then((res) => {
+      this.getUsers(); // new
+      this.setState({username: '', email: ''});
+    })
+    .catch((err) => {console.log(err); });
+  };
+
   // new
   componentDidMount(){
     this.getUsers();
@@ -29,10 +57,17 @@ class App extends Component {
       <section className="section">
         <div className="containers">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
             <br/>
             <h1 className="title is-1">All Users</h1>
             <hr/><br/>
+            <AddUser
+            username={this.state.username}
+            email={this.state.email}
+            addUser={this.addUser}
+            handleChange={this.handleChange} //new
+            />
+            <br/><br/>
             <UsersList users={this.state.users}/>
           </div>
         </div>
